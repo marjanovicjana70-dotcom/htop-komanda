f83ac1121ee3b576cab2503c61397fb89468b80b
marjanovicjana70-dotcom <marjanovicjana70@gmail.com>
2026-09-24 17:33:40 +0200

    zavrsena skripta

---
+++ README.md
@@ -0,0 +1,14 @@
+## JEDNOSTAVNIJA VERZIJA HTOP KOMANDE 
+
+<h4>Ova komanda je jako slicna htop komandi po tome sto nam daje uvid u kolicinu iskoriscenosti procesora u realnom vremenu. Za svaku nit (ili virtuelni procesor) graficki se prikazuje
+njena iskoriscenost u procentima.</h4>
+
+---
+
+Prebaciti ovu skriptu u /usr/local/bin kako bi bila prepoznatljiva sa bilo koje lokacije
+
+```
+sudo mv htop-komanda.sh /usr/local/bin
+```
+
+
--- src/htop-komanda.sh
+++ src/htop-komanda.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+ALL_CPUS=()
+CURR=()
+PREV=()
 
-declare -a ALL_CPUS
-declare -a PREV
-declare -a CURR
+
+BAR_LEN=30
+BAR_CHAR='|'
+EMPTY=' '
 
 if [[ -z "$NO_COLOR" ]]; then
 
@@ -27,7 +31,7 @@ function curr_to_prev(){
 PREV=()
 
 local key val 
-for "$key" in "${!CURR[@]}";
+for key in "${!CURR[@]}";
 do
 val="${CURR[$key]}"
 PREV[$key]="$val"
@@ -43,22 +47,23 @@ local irq softirq steal guest guest_nice
 
 CURR=()
 
-for "$key" in "${ALL_CPUS[@]}";
+for key in "${ALL_CPUS[@]}";
 do
 
-CURR[$key]=
+CURR[key]=
 
 done
 
-while read -r key user nice system idle iowait \ irq softirq guest guest_nice;
-do
+local cpu_number time_busy time_idle value
 
-if [[ "$key" != "cpu*" ]];
+while read -r key user nice system idle iowait irq softirq steal guest guest_nice; do
+
+if [[ "$key" != cpu* ]];
 then
 
 continue
 
-elif [[ "$key" == "cpu" ]];
+elif [[ "$key" == 'cpu' ]];
 then
 
 continue
@@ -67,7 +72,14 @@ fi
 
 
 
+cpu_number="${key#cpu}"
 
+time_busy=$((user + nice + system + irq + softirq + steal + guest + guest_nice))
+time_idle=$((idle + iowait))
+
+value="$time_busy $time_idle"
+
+CURR[$cpu_number]="$value"
 
 done < /proc/stat
 
@@ -75,24 +87,167 @@ done < /proc/stat
 
 }
 
-function cpu_count(){
 
-ALL_CPUS=(/sys/devices/virtual/cpuid/cpu[0-9]*)
-ALL_CPUS=("${ALL_CPUS[@]##*/cpu}")
+function print_bar(){
 
+local key=$1
+
+local busy1 idle1 busy2 idle2 
+
+read -r busy1 idle1 <<< "${PREV[$key]}"
+read -r busy2 idle2 <<< "${CURR[$key]}"
+
+
+local usage active
+
+if [[ -z $busy1 || -z $busy2 ]];
+then
+
+usage=0
+online=
+
+else
+
+local busySub=$(( busy2 - busy1 ))
+local idleSub=$(( idle2 - idle1 ))
+local total=$((busySub + idleSub))
+
+
+usage=$(( 1000 * busySub / total))
+
+online=1
+
+fi
+
+
+local int=$((usage / 10))
+local frac=$((usage % 10))
+
+local perc=$int.$frac
+
+local bars=$((usage * BAR_LEN / 1000))
+
+
+local i 
+local s='['
+
+for ((i = 0; i < bars; i++));
+do
+
+s+=$COLOR1$BAR_CHAR$RST
+
+done
+
+for ((i=bars; i<BAR_LEN; i++));
+do
+
+s+=$EMPTY
+
+done
+
+s+=']'
+
+local state
+if [[ -n $online ]];
+then
+
+printf -v perc '%-7s' "$perc%"
+		state=$COLOR3$perc$RST
+
+
+else 
+
+state=${DIM}offline$RST
+
+fi
+
+
+echo "$s ${COLOR2}cpu$key$RST $state"
+
+}
+
+
+
+function visualize(){
+
+echo "${DIM}CPU Usage on $COLOR3$HOSTNAME$RST"
+
+local key
+for key in "${!CURR[@]}";
+do
+
+print_bar "$key"
+
+done
+local now
+printf -v now '%(%Y-%m-%dT%H:%M:%S%z)T'
+echo "$DIM$now$RST"
 
 
 }
+
+function refresh(){
+
+printf '\e[?1049l' 
+printf '\e[?25h'
+
+}
+
+function updatews(){
+
+COLUMNS=${COLUMNS:-80}
+
+local all_cpus=("${!CURR[@]}")
+local highest="${all_cpus[-1]}"
+
+local i=$((2 + 4 + ${#highest} + 8))
+	BAR_LEN=$((COLUMNS - i))
+
+}
+
+function cpu_count(){
+    ALL_CPUS=(/sys/devices/virtual/cpuid/cpu[0-9]*)
+    ALL_CPUS=("${ALL_CPUS[@]##*/cpu}")
+}
+
 
 
 function main(){
 
+shopt -s checkwinsize
+
+
+cpu_count
+proc_info
+echo "waiting.."
+sleep 1
+
+
+trap refresh EXIT
+trap updatews WINCH
+
+printf '\e[?1049h' 
+printf '\e[?25l' 
+printf '\e[H' 
+
+updatews  
+
+while true;
+do
 
 curr_to_prev
 proc_info
+
+printf "\e[H"
+
+visualize
+
+sleep 1
+
+done
 
 
 }
 
 
-#main "$@"
+main "$@"
